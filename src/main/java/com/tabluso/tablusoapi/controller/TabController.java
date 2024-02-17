540199af2033f23e945e8b256f5a1797e2978d72
TIAGO\tiago <tiagomcsousa@outlook.com>
2024-02-17 21:17:24 +0000

    Fixes TabController base RequestMapping URL

--- src/main/java/com/tabluso/tablusoapi/controller/TabController.java
+++ src/main/java/com/tabluso/tablusoapi/controller/TabController.java
@@ -3,20 +3,19 @@ package com.tabluso.tablusoapi.controller;
 import com.tabluso.tablusoapi.entity.Tab;
 import com.tabluso.tablusoapi.repository.TabRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-//@RequestMapping("/tabs")
+@RequestMapping("/tabs")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @AllArgsConstructor
 public class TabController {
 
     private TabRepository tabRepository;
 
-    @GetMapping("/tabs")
+    @GetMapping
     public List<Tab> searchTabs(@RequestParam String searchText) {
         return tabRepository.findByArtistNameContainingOrSongNameContaining(searchText, searchText);
     }
